dc09f3d195d58305d4c25847e76dd4f13ad7a69e
novatori666 <42925457+novatori666@users.noreply.github.com>
2019-06-17 15:24:29 +0900

    info.js

--- src/commands/utility/info.js
+++ src/commands/utility/info.js
@@ -2,11 +2,10 @@
 const Discord = require('discord.js');
 module.exports = function info(bot, msg) {
     const embed = new Discord.RichEmbed();
-    embed.setTitle(':information_source: Alexi5 Status');
-    embed.setDescription('Here\'s some info about me');
+    embed.setTitle(':information_source: A Y U Status');
+    embed.setDescription('Here's some info about me');
     embed.addField(':white_check_mark: API Status', `ONLINE & bot latency to this server is ${Math.round(bot.ping)}ms`);
-    embed.addField(':spy:  My masters:', 'SYKC[https://github.com/SYKC] & Veegishx[https://github.com/VEEGISHx]');
-    embed.addField(':purple_heart:  My Life', 'I\'m currently being hosted on a Heroku free tier server');
+    embed.addField(':purple_heart:  My Life', 'I'm currently being hosted on a Heroku free tier server, please donate to make me alive longer.');
     embed.setColor('#00ff00');
     embed.addField(':satellite_orbital: Server dominance', `Found ${bot.guilds.size} server instances with a population of ${bot.users.size} users`);
     msg.channel.send(embed);
