f910cc73472c051349751aee7ccc74330ff6dc77
novatori666 <42925457+novatori666@users.noreply.github.com>
2019-06-17 14:25:52 +0900

    Update default.js

--- src/commands/default.js
+++ src/commands/default.js
@@ -23,20 +23,20 @@ module.exports = function listCommands(prefix, msg) {
         embed.addField(commands[i]);
     }
     */
-    embed.setTitle(':robot: Things I can do :robot: ')
-    embed.setDescription('More info: https://discordbots.org/bot/455038517983051816');
-    embed.addField(':dart:  FUN STUFF', '--------------');
-    embed.addField('Memes', `View a joke: ${prefix}meme`, true);
+    embed.setTitle(':emoji_17: Things I can do :emoji_17: ')
+    embed.setDescription('Huehuehuehue :emoji_18:');
+    embed.addField(':emoji_9:  FUN STUFF', '--------------');
+    embed.addField('Memes', `Request a meme: ${prefix}meme`, true);
     embed.addField('Jokes', `View a joke: ${prefix}joke`, true);
     embed.addField('Amazeme', `Be amazed: ${prefix}amazeme`, true);
     embed.addField('Music', `View music streaming help: ${prefix}music`, true);
-    embed.addField('Advice', `Ask for bot advice: ${prefix}ask`, true);
-    embed.addField(':tools: USEFUL STUFF', '--------------');
+    embed.addField('Advice', `Ask me for advice: ${prefix}ask`, true);
+    embed.addField(':emoji_29: USEFUL STUFF', '--------------');
     embed.addField('The Purge', `Purge messages: ${prefix}purge [num]`, true);
     embed.addField('Ping', `View latency to server: ${prefix}ping`, true);
-    embed.addField('Bot Status', `View bot info: ${prefix}info`, true);
-    embed.setColor('#2196f3');
-    embed.setFooter('Note: This bot is a WIP. Expect frequent updates!');
-    embed.setAuthor('Alexi5 Commands Help');
+    embed.addField('My Status', `View my info: ${prefix}info`, true);
+    embed.setColor('#0064ff');
+    embed.setFooter('Note: Please donate me to keep me going and add more feature!');
+    embed.setAuthor('A Y U v.0.b1');
     msg.channel.send(embed);
 }
