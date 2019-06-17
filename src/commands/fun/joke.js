1fde044aa4f66b7a61f1e36858701b8218061d57
novatori666 <42925457+novatori666@users.noreply.github.com>
2019-06-17 14:27:58 +0900

    joke.js

--- src/commands/fun/joke.js
+++ src/commands/fun/joke.js
@@ -1,6 +1,6 @@
 const got = require('got');
 module.exports = function joke(msg) {
-    got('https://www.reddit.com/r/jokes/random/.json').then(response => {
+    got('https://www.reddit.com/r/jokes/random.json').then(response => {
         let content = JSON.parse(response.body);
         var title = content[0].data.children[0].data.title;
         var joke = content[0].data.children[0].data.selftext;
